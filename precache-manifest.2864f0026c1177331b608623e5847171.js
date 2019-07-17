5e3ae3e69fd04d14a0abc36c1a3945a3b3cefea8
Alvin Jackson <alvinjackson@Alvins-MBP.attlocal.net>
2019-07-16 21:31:24 -0500

    Updates

---
+++ precache-manifest.2864f0026c1177331b608623e5847171.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2897471fd44c9cfb795b18a58db95a0",
+    "url": "/react-crash-courses/index.html"
+  },
+  {
+    "revision": "8cf30a4223fd8cb6a29f",
+    "url": "/react-crash-courses/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "45348780255338aa681c",
+    "url": "/react-crash-courses/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8cf30a4223fd8cb6a29f",
+    "url": "/react-crash-courses/static/js/2.eb088328.chunk.js"
+  },
+  {
+    "revision": "45348780255338aa681c",
+    "url": "/react-crash-courses/static/js/main.bacf7cb5.chunk.js"
+  },
+  {
+    "revision": "259181edecf4fbbbad00",
+    "url": "/react-crash-courses/static/js/runtime~main.16479ec5.js"
+  }
+]);
